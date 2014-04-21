b7c73b3ee29f8aa42cfd99b75fcfc1d7aa8d736e
Gany Chidambaram <ganapathy.c@gmail.com>
2014-04-20 19:38:07 -0700

    completed Adjacency Mat

---
+++ code/reconstruct/BhaGany/computeAdjMat.m
@@ -0,0 +1,21 @@
+function Am  = computeAdjMat(T)
+
+n = size(T,2);
+m = size(T,1);
+
+Am = zeros(n,n);
+
+for i = 1:m-1
+	timeSlice1 = T(i, :);
+        timeSlice2 = T(i+1,:);
+
+	for j = 1:n
+		if(timeSlice1(j)==1)
+			for k = 1:n
+				if(timeSlice2(k)==1)
+					Am(j,k) = Am(j,k) + 1;	
+				endif
+			endfor
+		endif
+	endfor  
+endfor
--- code/reconstruct/BhaGany/ourAlgo.m
+++ code/reconstruct/BhaGany/ourAlgo.m
@@ -1,5 +1,22 @@
 function score = ourAlgo(F, arg)
 
+disp("F = ");
+%disp(F);
+
+disp("arg = ");
+disp(arg);
+
+if nargin<3,
+    debug_ = false; 
+end
+
+[D, G] = discretizeFluorescenceSignal(F, 'debug', debug_, 'conditioningLevel', 0.25, 'bins', [-10,0.12,10]);
+
+disp("D  = ");
+disp(size(D));
+
+Am = computeAdjMat(D);
+
 %convert node values to binary
 %loop through binary matrix and add values to adjacency matrix.
 %second threshold to retain only the highly probable links.
