88002b73c31d7cd3ffbfcdbed3415218fde03ed9
Gany Chidambaram <ganapathy.c@gmail.com>
2014-04-20 17:25:24 -0700

    placeholder for our scoring method

---
+++ code/reconstruct/BhaGany/ourAlgo.m
@@ -0,0 +1,5 @@
+function score = ourAlgo(F, arg)
+
+%convert node values to binary
+%loop through binary matrix and add values to adjacency matrix.
+%second threshold to retain only the highly probable links.
